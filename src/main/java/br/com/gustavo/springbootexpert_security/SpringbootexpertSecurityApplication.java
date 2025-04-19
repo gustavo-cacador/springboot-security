50a2dcd22a9059ec912639321d1b99e9a7214187
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-04-19 15:07:09 -0300

    Initial commit

---
+++ src/main/java/br/com/gustavo/springbootexpert_security/SpringbootexpertSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.springbootexpert_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexpertSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexpertSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/springbootexpert_security/SpringbootexpertSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.springbootexpert_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexpertSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

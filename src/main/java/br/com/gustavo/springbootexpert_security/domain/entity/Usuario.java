0df4063ff6896a5dbadb592c3d4ae0a4b22e3686
Gustavo Caçador <gustavoirl456@outlook.com>
2025-04-24 16:37:32 -0300

    Criando a lógica do AuthenticationProvider do controle de acesso

--- src/main/java/br/com/gustavo/springbootexpert_security/domain/entity/Usuario.java
+++ src/main/java/br/com/gustavo/springbootexpert_security/domain/entity/Usuario.java
@@ -1,10 +1,9 @@
 package br.com.gustavo.springbootexpert_security.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -17,4 +16,8 @@ public class Usuario {
     private String login; // login -> cpf, email, nome de usuario
     private String senha;
     private String nome;
+
+    @Transient // ignora o atributo para o banco de dados
+    private List<String> permissoes;
+
 }
--- src/main/java/br/com/gustavo/springbootexpert_security/domain/repository/UsuarioRepository.java
+++ src/main/java/br/com/gustavo/springbootexpert_security/domain/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package br.com.gustavo.springbootexpert_security.domain.repository;
 import br.com.gustavo.springbootexpert_security.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    Optional<Usuario> findByLogin(String login);
 }
